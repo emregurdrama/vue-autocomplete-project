eadb177880bec88c2cc1c02f53aea3a829f0dc83
emregurdrama <recepata54@gmail.com>
2022-11-01 04:00:34 +0300

    Update service.js

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,6 @@
 
 export async function fetchData() {
-	const result = await fetch("https://raw.githubusercontent.com/Front-End-Bootcamp/vue-component-create-team-fox-2/main/src/data/data.json")
+	const result = await fetch(https://raw.githubusercontent.com/emregurdrama/vue-autocomplete-project/main/src/data/data.json")
 		.then((response) => response.json())
 		.then((data) => data)
 	return result;
